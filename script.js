6bb1dc141546e65170d845c539ca435a2c2342ad
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-05-02 21:56:01 +0100

    Created a JSON to store the video data

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-var currentTimeStamp = 0;
+var currentTimeStamp = "00.00";
 document.getElementById("myVideo").addEventListener('timeupdate', function() {
     var video = document.getElementById("myVideo");
     var hours = parseInt(video.currentTime / (60 * 60), 10);
     var minutes = parseInt(video.currentTime / 60, 10).toString().padStart(2, '0');
     var seconds = parseInt(video.currentTime % 60, 10).toString().padStart(2, '0');
     currentTimeStamp = minutes + "." + seconds;
+
     document.getElementById("timer").innerHTML = currentTimeStamp;
 
-    if(currentTimeStamp >= "00.23" && currentTimeStamp <= "00.34"){
-        document.getElementById("square").style.display = "block";
-    }
-
-    if(currentTimeStamp >= "00.34" || currentTimeStamp <= "00.23"){
-        document.getElementById("square").style.display = "none";
-    }
+    getCheckpoints();
 });
+
+function getCheckpoints() {
+    var videoSrc = document.getElementById("myVideo").src;
+    videoSrc = videoSrc.substring(videoSrc.indexOf("videos"));
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            for (var i = 1; i <= Object.keys(data).length; i++) {
+                var videoFile = data['video' + i].file;
+                if (videoFile === videoSrc) {
+                    var isWithinCheckpoint = false; // flag to track if timestamp is within any checkpoint
+                    for (var j = 0; j <= Object.keys(data['video' + i].checkpoints).length - 1; j++) {
+                        if (currentTimeStamp >= data['video' + i].checkpoints[j].start && currentTimeStamp <= data['video' + i].checkpoints[j].end) {
+                            isWithinCheckpoint = true;
+                            break; // exit the loop as soon as we find a matching checkpoint
+                        }
+                    }
+                    // set the display based on whether timestamp is within any checkpoint
+                    document.getElementById("square").style.display = isWithinCheckpoint ? "block" : "none";
+                }
+            }   
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
