37eac981654a4e3e37d0d52fbb33b433b4df8bea
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-05-30 15:17:47 +0100

    Started working on the videoPlayer modal

--- js/videos.js
+++ js/videos.js
@@ -1,13 +1,46 @@
+const playerVideos = new Plyr('video', {
+    controls: [
+        'play-large',
+        'rewind',
+        'play',
+        'fast-forward',
+        'progress',
+        'current-time',
+        'duration',
+        'mute',
+        'volume',
+        'fullscreen'
+    ],
+});
+window.player = playerVideos; // Expose player so it can be used from the console
+
 window.addEventListener('load', () => {
     const linkText = document.querySelector('.container-video-info-text p');
     const linkIcon = document.querySelector('.container-video-info-text svg');
     const linkTextHeight = window.getComputedStyle(linkText).lineHeight;
     console.log(linkTextHeight);
     linkIcon.style.height = linkTextHeight;
+
+    const playerDiv = document.querySelector('.plyr');
+    playerDiv.style.borderRadius = '12px';
+    playerDiv.style.boxShadow = 'none';
 });
+
+const modalVideoPlayer = document.getElementById('video-player-modal');
 
 const video1 = document.getElementById('video1');
 video1.style.backgroundImage = 'url(../resources/videos/video1poster.webp)';
+
+video1.addEventListener('click', () => {
+    if (modalVideoPlayer.style.display === 'none') {
+        modalVideoPlayer.style.display = 'block';
+        document.body.classList.add('modal-open');
+    }
+    else {
+        modalVideoPlayer.style.display = 'none';
+        document.body.classList.remove('modal-open');
+    }
+});
 
 const video2 = document.getElementById('video2');
 video2.style.backgroundImage = 'url(../resources/videos/video2poster.webp)';
